a4f566ef642fdeef70fff109f5dcec71d87d196a
gabdelaun <gabdelaun@gmail.com>
2016-11-08 16:37:04 +0000

    Add

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,31 @@
+class ArticleController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+        @article = Article.find(params[:id])
+  end
+
+  def new
+      @article = Article.new
+  end
+
+  def create
+        @article = Article.new(params[:article])
+    @article.save
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+     @article = Article.find(params[:id])
+    @article.update(params[:article])
+  end
+
+  def destroy
+     @article = Article.find(params[:id])
+  end
+end
